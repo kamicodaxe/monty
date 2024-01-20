ee56748a4ed7d69c9cf76c5f87fc9d5e18128ea2
Loïc Kami wsl <lkami@bogital.com>
2024-01-20 02:58:32 +0100

    Add find_instructions

---
+++ utils_instructions.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * find_instruction - Find the function pointer for the given opcode.
+ * @opcode: Opcode to search for.
+ * Return: Function pointer if found, NULL otherwise.
+ */
+void (*find_instruction(char *opcode))(char *, char *, unsigned int)
+{
+	size_t i;
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		/* Add other instructions as needed */
+		{NULL, NULL} /* Terminate the array with NULL values */
+	};
+
+	for (i = 0; instructions[i].opcode; i++)
+	{
+		if (strcmp(instructions[i].opcode, opcode) == 0)
+			return (instructions[i].f);
+	}
+
+	fprintf(stderr, "Error: Unknown opcode %s\n", opcode);
+	exit(EXIT_FAILURE);
+}
