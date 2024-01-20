d2f2e715a397290bc7736e3487375986628b6615
Loïc Kami wsl <lkami@bogital.com>
2024-01-20 02:59:08 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,38 @@
 #include "monty.h"
+stack_t *stack = NULL;
 
+/**
+ * main - Entry point for the Monty interpreter.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ *
+ * Description: Validates command-line arguments, opens and reads the Monty
+ * bytecode file, and interprets the instructions.
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure.
+ */
 int main(int argc, char **argv)
 {
-	char *m_file_path = NULL;
+	char *data = NULL;
 	int fd;
 
 	if (argc != 2)
-	{
-		perror("USAGE: monty file");
-		exit(EXIT_FAILURE);
-	}
-
-	m_file_path = *(argv + 1); /* Access next string in array */
+		return (error_usage());
 
 	/* Open monty file path */
-	fd = open_file(m_file_path);
-	read_bytes(fd);
+	fd = open_file(argv[1]);
+	if (fd == -1)
+		return (error_open_file(argv[1]));
 
-	return (0);
+	if ((read_bytes(fd, &data) == -1))
+		return (error_open_file(argv[1]));
+
+	/* Parse monty file */
+	parse_monty_file(data);
+
+	/* Free allocated memory and exit */
+	free_stack(stack);
+	free(data);
+
+	return (EXIT_SUCCESS);
 }
